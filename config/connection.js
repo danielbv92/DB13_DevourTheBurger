b076e864524c9775f7ba36612d8ab06177d63405
Daniel Beltran <dannybv92@gmail.com>
2020-12-14 15:20:01 -0800

    added var instead of const

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql");
+var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
@@ -11,5 +11,11 @@ if (process.env.JAWSDB_URL) {
     database: "burgers_db"
   });
 }
-connection.connect();
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 module.exports = connection;
